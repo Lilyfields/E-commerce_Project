3ad0dedeada18a50d054867c767c2d759660fa6d
Lilyfields <lilyfields11@bigpond.com>
2022-04-06 21:54:40 +1000

    tested route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,16 +7,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all products
   try {
-    const productData = await Product.findAll({
-      include: [{model:Tag}],
+    const products = await Product.findAll({
+      include: [{ model: Tag }],
     });
-    res.status(200).json (productData);
-   }catch (err) {
-     res.status(500).json(err);
-   }
-     // be sure to include its associated Products
-   });
-  // be sure to include its associated Category and Tag data
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  // be sure to include its associated Products
+});
+// be sure to include its associated Category and Tag data
 
 
 // get one product
@@ -33,14 +33,20 @@ router.get('/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-  // be sure to include its associated Category and Tag data
+// be sure to include its associated Category and Tag data
 
 // create new product
-router.post('/', async (req, res) => {
-
-  try{
-    const productData = await Product.create(req.body);
-    then((product) => {
+router.post('/', (req, res) => {
+  /* req.body should look like this...
+    {
+      product_name: "Basketball",
+      price: 200.00,
+      stock: 3,
+      tagIds: [1, 2, 3, 4]
+    }
+  */
+  Product.create(req.body)
+    .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -51,24 +57,6 @@ router.post('/', async (req, res) => {
         });
         return ProductTag.bulkCreate(productTagIdArr);
       }
-  
-  /* req.body should look like this...
-  
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
- 
-    
       // if no product tags, just respond
       res.status(200).json(product);
     })
@@ -77,6 +65,8 @@ router.post('/', async (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
+});
+
 
 // update product
 router.put('/:id', (req, res) => {
@@ -120,8 +110,15 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',async (req, res) => {
   // delete one product by its `id` value
+  try {
+    await Product.destroy({ where: { id: req.params.id } });
+
+    res.status(200).end();
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
